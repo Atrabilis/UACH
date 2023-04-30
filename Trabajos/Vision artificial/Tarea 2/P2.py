df2b79e5c0fe5d553da8f2b2eb7a7578730f57e8
Atrabilis <angel.bruno.andrade.osorio@gmail.com>
2023-04-30 16:45:40 -0400

    Add P2 to Vision Artificial

---
+++ Trabajos/Vision artificial/Tarea 2/P2.py
@@ -0,0 +1,21 @@
+#Se importan librerias
+import os
+import cv2 
+import numpy as np 
+
+
+
+#Borra terminal en windows
+os.system('cls')
+
+# Pregunta a usuario por el factor del zoom
+factor = int(input("Ingrese el factor del zoom a aplicar en la imagen:"))
+ 
+# Create point matrix get coordinates of mouse click on image
+point_matrix = np.zeros((2,2),int)
+
+img = cv2.imread(os.path.dirname(__file__) + '\imagen1.jpg')
+
+cv2.imshow("asdfasdf", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
