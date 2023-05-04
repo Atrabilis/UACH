8ca48adde2b76adb81edef2932487372df2a3c67
Atrabilis <angel.bruno.andrade.osorio@gmail.com>
2023-05-03 23:39:13 -0400

    Se crea notebook para pruebas y se crea matriz ampliada de ceros en P2

--- Trabajos/Vision artificial/Tarea 2/P2.py
+++ Trabajos/Vision artificial/Tarea 2/P2.py
@@ -14,6 +14,18 @@ point_matrix = np.zeros((2,2),int)
 
 img = cv2.imread(os.path.dirname(__file__) + '\imagen1.jpg')
 
+#Máscara de convolución utilizada en el proceso de zoom
+mascara_conv = np.array([[0.25,0.5 ,0.25],[0.5,1 ,0.5],[0.25,0.5 ,0.25]])
+
+#proceso para crear la matriz ampliada con ceros
+filaDeCeros =np.zeros((1,img.shape[1],img.shape[2]), dtype = img.dtype)
+ampliada = np.insert(img, np.s_[::1], filaDeCeros, axis=0)
+ampliada = np.vstack((ampliada, filaDeCeros))
+columnaDeCeros = np.zeros((ampliada.shape[0],1,img.shape[2]), dtype = ampliada.dtype)
+ampliada = np.insert(ampliada,np.s_[0::1],columnaDeCeros, axis = 1)
+ampliada = np.hstack((ampliada,columnaDeCeros))
+
+
 cv2.imshow("asdfasdf", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
