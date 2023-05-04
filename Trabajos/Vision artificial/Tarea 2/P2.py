32b49e98ea459601f61dd62cf617f080d60c66f1
Atrabilis <angel.bruno.andrade.osorio@gmail.com>
2023-05-04 01:06:16 -0400

    avance P2

--- Trabajos/Vision artificial/Tarea 2/P2.py
+++ Trabajos/Vision artificial/Tarea 2/P2.py
@@ -12,6 +12,7 @@ factor = int(input("Ingrese el factor del zoom a aplicar en la imagen:"))
 # Create point matrix get coordinates of mouse click on image
 point_matrix = np.zeros((2,2),int)
 
+#Captura la imagen
 img = cv2.imread(os.path.dirname(__file__) + '\imagen1.jpg')
 
 #Máscara de convolución utilizada en el proceso de zoom
@@ -26,6 +27,8 @@ ampliada = np.insert(ampliada,np.s_[0::1],columnaDeCeros, axis = 1)
 ampliada = np.hstack((ampliada,columnaDeCeros))
 
 
-cv2.imshow("asdfasdf", img)
+
+
+cv2.imshow("imagen Original", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
