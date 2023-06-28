c79f0c70e5f7f430e6397477f5458063f4a3318c
Angel <angel.bruno.andrade.osorio@gmail.com>
2023-06-27 22:47:49 -0400

    otro avance proyecto

--- Trabajos/Vision artificial/Proyecto/labeling.py
+++ Trabajos/Vision artificial/Proyecto/labeling.py
@@ -2,8 +2,10 @@ import os
 import csv
 import random
 
+os.system("cls")
+
 # Ruta de la carpeta que contiene las imágenes
-carpeta_imagenes = 'DB2_B'
+carpeta_imagenes = 'Dataset'
 
 # Obtener la lista de archivos en la carpeta
 archivos = os.listdir(carpeta_imagenes)
@@ -20,7 +22,7 @@ for archivo in archivos:
     etiqueta = int(archivo[:3])
     
     # Agregar la ruta de archivo, nombre de archivo y etiqueta a la lista
-    datos.append([ruta, archivo, etiqueta])
+    datos.append([ruta, archivo, etiqueta -101])
 
 # Guardar los nombres de archivo, rutas y etiquetas en un archivo CSV
 with open('etiquetas.csv', 'w', newline='') as csvfile:
