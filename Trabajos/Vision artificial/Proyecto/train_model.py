a0dc50746e770c1e9c02134a62bec3569fea15cb
Angel <angel.bruno.andrade.osorio@gmail.com>
2023-07-02 18:29:38 -0400

    funcionalidad parar entrenamiento f10

--- Trabajos/Vision artificial/Proyecto/train_model.py
+++ Trabajos/Vision artificial/Proyecto/train_model.py
@@ -7,6 +7,7 @@ from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import keyboard
 
 # Limpiar la consola
 os.system('cls' if os.name == 'nt' else 'clear')
@@ -48,22 +49,30 @@ test_labels = tf.keras.utils.to_categorical(test_data['Etiqueta'])
 # Crear el modelo de la red neuronal
 model = Sequential()
 model.add(Flatten(input_shape=train_images.shape[1:]))  # Aplanar la imagen en un vector
-model.add(Dense(128, activation='relu'))
+model.add(Dense(64, activation='relu'))
 model.add(Dense(64, activation='relu'))  # Capa oculta con 64 neuronas
 model.add(Dense(40, activation='softmax'))  # Capa de salida con activación softmax
 
 # Compilar el modelo
 model.compile(optimizer=Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
 
+# Definir una función para detener el entrenamiento
+def stop_training():
+    model.stop_training = True
+
+# Asignar la función stop_training a la tecla F10
+keyboard.on_press_key("f10", lambda _: stop_training())
+
 # Entrenar el modelo
-history = model.fit(train_images, train_labels, epochs=1000, batch_size=32, validation_data=(val_images, val_labels))
+history = model.fit(train_images, train_labels, epochs=500, batch_size=64, validation_data=(val_images, val_labels),
+                    callbacks=[tf.keras.callbacks.LambdaCallback(on_epoch_end=lambda epoch, logs: stop_training() if model.stop_training else None)])
 
 # Evaluar el modelo en el conjunto de prueba
 #loss, accuracy = model.evaluate(test_images, test_labels)
 #print('Test loss:', loss)
 #print('Test accuracy:', accuracy)
 
-# Mostrar el historial de precisión de entrenamiento y prueba
+# Mostrar el gráfico del historial de precisión
 plt.plot(history.history['accuracy'])
 plt.plot(history.history['val_accuracy'])
 plt.title('Model Accuracy')
