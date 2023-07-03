d03aea390d1ffd0bf05aeee6abf50c331cee8241
Angel <angel.bruno.andrade.osorio@gmail.com>
2023-07-02 22:16:35 -0400

    added model test

---
+++ Trabajos/Vision artificial/Proyecto/test_model.py
@@ -0,0 +1,42 @@
+import os
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+
+# Cargar el modelo guardado
+model = load_model('modelo.h5')
+
+# Función para cargar y preprocesar la imagen
+def load_and_preprocess_image(image_path):
+    image = load_img(image_path, target_size=(50, 50))  # Asegurarse de que la imagen tenga el mismo tamaño que las imágenes de entrenamiento
+    image_array = img_to_array(image) / 255.0  # Normalizar los valores de píxel entre 0 y 1
+    return image_array
+
+# Directorio que contiene las imágenes
+dataset_dir = 'Procesadas'
+
+# Obtener la lista de archivos en el directorio
+image_files = os.listdir(dataset_dir)
+
+# Iterar sobre cada imagen y realizar la predicción
+for image_file in image_files:
+    # Construir la ruta completa de la imagen
+    image_path = os.path.join(dataset_dir, image_file)
+    
+    # Cargar y preprocesar la imagen
+    image = load_and_preprocess_image(image_path)
+    
+    # Realizar la predicción
+    predictions = model.predict(np.expand_dims(image, axis=0))
+    
+    # Obtener la clase predicha (índice con mayor probabilidad)
+    predicted_class = np.argmax(predictions)
+    
+    # Obtener la probabilidad asociada a la clase predicha
+    confidence = predictions[0][predicted_class]
+    
+    # Imprimir los resultados
+    print('Imagen:', image_file)
+    print('Clase predicha:', predicted_class)
+    print('Probabilidad:', confidence)
+    print('---')
--- Trabajos/Vision artificial/Proyecto/train_model.py
+++ Trabajos/Vision artificial/Proyecto/train_model.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten, Dropout
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import os
 import numpy as np
 import matplotlib.pyplot as plt
 import keyboard
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, classification_report
 
 # Limpiar la consola
 os.system('cls' if os.name == 'nt' else 'clear')
@@ -50,7 +51,7 @@ test_labels = tf.keras.utils.to_categorical(test_data['Etiqueta'])
 model = Sequential()
 model.add(Flatten(input_shape=train_images.shape[1:]))  # Aplanar la imagen en un vector
 #model.add(Dropout(.5))
-model.add(Dense(512, activation='relu'))
+model.add(Dense(256, activation='relu'))
 #model.add(Dropout(.5))
 model.add(Dense(256, activation='relu'))
 #model.add(Dropout(.5))
@@ -85,10 +86,42 @@ model.compile(optimizer=Adam(), loss='categorical_crossentropy', metrics=['accur
 history = model.fit(train_images, train_labels, epochs=1000, batch_size=1024, validation_data=(val_images, val_labels),
                     callbacks=[StopTrainingCallback()])
 
-# Evaluar el modelo en el conjunto de prueba
-#loss, accuracy = model.evaluate(test_images, test_labels)
-#print('Test loss:', loss)
-#print('Test accuracy:', accuracy)
+
+# Guardar el modelo
+
+# Cargar los datos y las etiquetas verdaderas
+test_data = pd.read_csv('test_data.csv')
+test_images = np.array([load_and_preprocess_image(image_path) for image_path in test_data['Ruta']])
+test_labels = tf.keras.utils.to_categorical(test_data['Etiqueta'])
+
+# Obtener las predicciones y la pérdida del modelo
+loss, accuracy = model.evaluate(test_images, test_labels)
+
+# Obtener las predicciones del modelo
+predictions = model.predict(test_images)
+predicted_labels = np.argmax(predictions, axis=1)
+
+# Calcular las métricas
+accuracy = accuracy_score(np.argmax(test_labels, axis=1), predicted_labels)
+precision = precision_score(np.argmax(test_labels, axis=1), predicted_labels, average='macro')
+recall = recall_score(np.argmax(test_labels, axis=1), predicted_labels, average='macro')
+f1 = f1_score(np.argmax(test_labels, axis=1), predicted_labels, average='macro')
+confusion = confusion_matrix(np.argmax(test_labels, axis=1), predicted_labels)
+classification = classification_report(np.argmax(test_labels, axis=1), predicted_labels)
+loss, accuracy = model.evaluate(test_images, test_labels)
+
+# Imprimir las métricas
+print('Test loss:', loss)
+print('Test accuracy:', accuracy)
+print("Loss:", loss)
+print("Accuracy:", accuracy)
+print("Precision:", precision)
+print("Recall:", recall)
+print("F1-Score:", f1)
+print("Confusion Matrix:\n", confusion)
+print("Classification Report:\n", classification)
+
+model.save('modelo.h5')
 
 # Mostrar el gráfico del historial de precisión
 plt.plot(history.history['accuracy'])
@@ -99,5 +132,4 @@ plt.ylabel('Accuracy')
 plt.legend(['Train', 'Validation'], loc='upper left')
 plt.show()
 
-# Guardar el modelo
-model.save('modelo.h5')
+
